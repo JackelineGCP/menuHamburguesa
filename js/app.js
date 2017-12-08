18406b71f06aac4e34bf75930130fbd4cfdc24c4
JackelineGCP <gessenia_17@hotmail.com>
2017-12-08 17:39:28 -0500

    agrega menu de hamburgesa

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
-var boton = document.getElementById("btn")
+var boton = document.getElementById("btn");
+boton.addEventListener("click", showMenu);
 function showMenu(){
-    var menu = document.getElementById("opcs-menu");
-    if(menu.classList.contains("disabled-menu")){
-        console.log("mostrar");
-        menu.classList.remove("disabled-menu");
-        menu.classList.add("enabled-menu");
+	var menu = document.getElementById("opcs-menu");
+	if(menu.classList.contains("disabled-menu")){
+		menu.classList.remove("disabled-menu");
+		menu.classList.add("enabled-menu");
     }else{
-        menu.classList.remove("enabled-menu");
-        menu.classList.add("disabled-menu");
+			menu.classList.remove("enabled-menu");
+			menu.classList.add("disabled-menu");
     }
 }
-boton.addEventListener("click",showMenu);
